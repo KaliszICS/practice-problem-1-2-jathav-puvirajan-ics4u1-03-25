306be2e8cdcedc4a42791c030c44cb47e813725a
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-03-15 20:57:59 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,30 @@ public class PracticeProblem {
 
 	}
 
-	
-
+	public static boolean validIndex(int[] arr, int i) {
+		try {
+			int element = arr[i];
+			return true;  
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+	public static int divide(int a, int b) {
+		int result = 0;
+		try {
+			 result = a / b;
+		} catch (ArithmeticException e) {
+			result = 0;
+		}
+		return result;
+	}
+	public static int safeConvertStringtoInt(String s) {
+		int output;
+		try {
+			 output = Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			output = 0;
+		}
+		return output;
+	}
 }
